cbaac6c8e3f8280e50e3623303b0980802dff64b
aysha789 <ayshasiddika0102636@gmail.com>
2025-03-22 16:39:51 +0600

    full from

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool text= true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,23 +19,108 @@ class _HomePageState extends State<HomePage> {
             style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold),),
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 15,),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: TextField(
-             // textInputAction: TextInputAction.search,
-             // keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-
-                )
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 15,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Name",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  SizedBox(height: 20,),
+                  TextField(
+                    // textInputAction: TextInputAction.search,
+                    // keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      hintText: "Name",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color:Colors.yellowAccent,width: 10)
+                        )
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text("Password",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    obscureText:text,
+                    decoration: InputDecoration(
+                      suffixIcon:InkWell(onTap: (){
+                        text=!text;
+                        setState(() {7
+                        });
+                      },
+                          child: text? Icon(Icons.visibility):Icon(Icons.visibility_off)),
+                      hintText: "Password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color:Colors.yellowAccent,width: 10
+                        ),
+        
+        
+                      )
+                    ),
+                  ),
+                  Text("Father's Name",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                            color:Colors.yellowAccent,width: 10
+                        )
+                      )
+                    ),
+                  ),
+                  Text("Mother's Name",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                                color:Colors.yellowAccent,width: 10
+                            )
+                        )
+                    ),
+                  ),
+                  Text("Address",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                  SizedBox(
+                    height: 20,
+                  ),
+                TextField(
+                  maxLines: 6,
+                  decoration: InputDecoration(
+                    hintText: "Address",
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                          color:Colors.yellowAccent,width: 10
+                      ),
+                    )
+                  ),
+                ),
+                  SizedBox(height: 20,),
+                  TextButton(
+        
+                      onPressed: (){}, child: Text("Submit"))
+                ],
+              )
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
